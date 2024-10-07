e7cc847e72024ba3e3322528c315da923247ad88
lalchi <lalchimi@hotmail.fr>
2024-10-07 16:01:28 +0200

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+public class App {
+
+  /**
+  * @param args
+  */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Bienvenue dans le Gestionnaire de Budget");
+  }
+
+}
